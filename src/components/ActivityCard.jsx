441cef0cbc3e7b31e5b5a7ca039ee1bf552d8172
Adriel <Tempest20a@gmail.com>
2026-08-20 21:12:17 +0800

    style: add top margin to ActivityCard

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -1,6 +1,6 @@
 export default function ActivityCard({ title, subtitle, color, children }) {
   return (
-    <div className="border-2 border-l-4 border-b-4 max-w-md mx-auto">
+    <div className="mt-8 border-2 border-l-4 border-b-4 max-w-md mx-auto">
       <div className={`${color} p-4`}>
         <h1 className="font-medium text-xl">{title}</h1>
         <h2>{subtitle}</h2>
