d59730b73faebbfc52cf004ad94e975b6fa6465e
Adriel <Tempest20a@gmail.com>
2026-08-20 22:09:54 +0800

    style: improve navbar mobile ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,64 @@
 import { NavLink } from "react-router";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "Activity 1", label: "Login Authentication", path: "/login" },
+    { name: "Activity 2", label: "Grade Evaluation", path: "/grade-evaluation" },
+    { name: "Activity 3", label: "Password Checker", path: "/password-checker" },
+    { name: "Activity 4", label: "Electiricty Bill", path: "/electricity-bill" },
+    { name: "Activity 5", label: "Attendance Checker", path: "/attendance-checker" },
+  ];
+
   const navClass = ({ isActive }) =>
     isActive
-      ? "text-blue-500 text-sm font-medium"
-      : "hover:text-blue-500  text-gray-600 transition-colors text-sm font-medium ";
+      ? "text-black text-sm font-medium"
+      : "hover:text-gray-400 text-gray-600 transition-colors text-sm font-medium";
 
   return (
-    <nav className="sticky top-0 max-w-7xl mx-auto flex w-full items-center py-4">
-      <NavLink to="/" className="text-xl font-medium">
-        React Activity Portal
-      </NavLink>
+    <nav className="sticky top-0 z-10 mx-auto w-full max-w-7xl bg-white p-4">
+      <div className="flex items-center">
+        <NavLink to="/" className="text-xl font-medium">
+          React Activity Portal
+        </NavLink>
 
-      <ul className="ml-auto flex gap-8">
-        <li>
-          <NavLink to="/" className={navClass}>
-            Home
-          </NavLink>
-        </li>
+        <ul className="ml-auto hidden gap-8 md:flex">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <NavLink to={item.path} className={navClass}>
+                {item.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-        <li>
-          <NavLink to="/login" className={navClass}>
-            Activity 1
-          </NavLink>
-        </li>
+        <button
+          className="ml-auto md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+      </div>
 
-        <li>
-          <NavLink to="/grade-evaluation" className={navClass}>
-            Activity 2
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/password-checker" className={navClass}>
-            Activity 3
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/electricity-bill" className={navClass}>
-            Activity 4
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/attendance-checker" className={navClass}>
-            Activity 5
-          </NavLink>
-        </li>
-      </ul>
-
-      <button className="md:hidden">
-        <Menu size={24} color="#333" strokeWidth={2} />
-      </button>
+      {isOpen && (
+        <ul className="mt-4 flex flex-col text-center gap-4 md:hidden ">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <NavLink
+                to={item.path}
+                className={navClass}
+                onClick={() => setIsOpen(false)}
+              >
+                {item.name} {item.label && `- ${item.label}`}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 }
